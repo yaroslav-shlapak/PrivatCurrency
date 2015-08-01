c73668988224031a96ca7350b80b0f4a08966f26
Yaroslav <yarosalv.shlapak@gmail.com>
2015-08-01 16:18:35 +0300

    Save

---
+++ app/src/main/java/com/voidgreen/privatcurrency/Constants.java
@@ -0,0 +1,9 @@
+package com.voidgreen.privatcurrency;
+
+/**
+ * Created by Void on 31-Jul-15.
+ */
+public class Constants {
+    public static final String EXCHANGE_RATE_CARD = "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11";
+    public static final String EXCHANGE_RATE_CASH = "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5";
+}
---
+++ app/src/main/java/com/voidgreen/privatcurrency/CurrencyProvider.java
@@ -0,0 +1,9 @@
+package com.voidgreen.privatcurrency;
+
+/**
+ * Created by Void on 31-Jul-15.
+ */
+public class CurrencyProvider {
+
+
+}
